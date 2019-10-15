56517eff20d29a56a3e9fb32c20dcc0e57908347
Igor <igordjukic0@gmail.com>
2019-10-15 12:47:16 +0200

    removed public folder

--- public/styles-24e637ead7e44567a7e0.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{142:function(n,w,o){}}]);
-//# sourceMappingURL=styles-24e637ead7e44567a7e0.js.map
--- public/styles-826349502dc9a60a047b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{142:function(n,w,o){}}]);
-//# sourceMappingURL=styles-826349502dc9a60a047b.js.map
