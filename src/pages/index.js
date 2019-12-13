c73625f322a4426d510a5bdf79ba241221c4d055
Igor <igordjukic0@gmail.com>
2019-12-13 23:57:44 +0100

    Added react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,13 @@ import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import Body from "../components/body"
+import { Helmet } from "react-helmet"
 
 export default () => (
   <div className="main">
+    <Helmet>
+      <title>Igor Đukić</title>
+    </Helmet>
     <Header />
     <Body />
     <Footer />
