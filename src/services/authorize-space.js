4834f45c3404d6721e850de2f9e6c360ee4dc5cd
Igor <igordjukic0@gmail.com>
2020-04-05 16:56:41 +0200

    Fixed not defined error when deploying

--- src/services/authorize-space.js
+++ src/services/authorize-space.js
@@ -4,11 +4,11 @@ const AuthorizeSpace = () => {
   const reducer = (state, action) => {
     if (action === 'test') {
       state = !!action
-      window.localStorage.setItem('authorized', state)
+      typeof window !== 'undefined' && window.localStorage ? window.localStorage.setItem('authorized', state) : null
     }
     return state
   }
-  const [isAuthorized, authorize] = useReducer(reducer, window.localStorage.getItem('authorized'));
+  const [isAuthorized, authorize] = useReducer(reducer, typeof window !== 'undefined' && window.localStorage ? window.localStorage.getItem('authorized') : null);
 
   return { isAuthorized, authorize };
 }
